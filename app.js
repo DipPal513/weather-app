c9d77db9fd7376c6e687b489be74f0e4059bdaf3
DipPal513 <65772938+DipPal513@users.noreply.github.com>
2021-08-29 15:21:21 +0600

    4

---
+++ app.js
@@ -0,0 +1,44 @@
+
+// gloabal variable
+
+const input = document.querySelector('input');
+const searchBtn = document.querySelector('search-btn');
+
+
+if(input.value == ''){
+    
+}
+ 
+const searchWeather = () =>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=40efc1d2374da0d8848db007f252200f`)
+        .then(res => res.json())
+        .then(data => displayWeather(data))
+}
+
+const displayWeather =(data)=>{
+    const displayWeather = document.getElementById('displayWeather');
+    console.log(data)
+    const div = document.createElement('div');
+    div.className = 'text-center '
+    div.innerHTML = 
+    `
+    <div class="card" style="width: 30rem; backdrop-filter:blur(20px); color:#fff;padding:30px; background:#ffffff26;>
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${data.name}</h5>
+            <h5>${data.sys.country}</h5>
+            <p>Timezone : ${data.timezone}</p>
+            <p>Temp : ${data.main.temp}</p>
+            
+            </div>
+            <div class="card-body">
+            <p class="">weather :${data.weather[0]. description}</p>
+                     <p>Feels Like :${data.main.feels_like}</p>
+                    </div>
+                  </div> 
+    `
+    input.value = ''
+    displayWeather.textContent = '';
+    displayWeather.appendChild(div)
+
+}
